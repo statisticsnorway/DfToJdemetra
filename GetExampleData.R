052720c56bd172b07af165ed6b98f0f47e915e66
Øyvind Bruer-Skarsbø <obr@ssb.no>
2022-03-09 13:06:43 +0000

    First commit

---
+++ GetExampleData.R
@@ -0,0 +1,25 @@
+# Loaded packages
+library(PxWebApiData)
+library(tidyr)
+
+# Get data from Statistics Norways API about the number of jobs per month. Preliminary figures. 
+Jobs <- ApiData2("http://data.ssb.no/api/v0/en/table/13126",
+                 Tid = TRUE,
+                 ForelopigEndelig = '01',
+                 ContentsCode = 'AntArbForhold',
+                 NACE2007 = TRUE)
+
+# Filter out totals and remove unnecessary columns 
+JobsPrelim <- Jobs[Jobs$NACE2007 != "00-99",
+                  -which(names(Jobs) %in% c("ForelopigEndelig", "ContentsCode"))]
+
+# Get the data on wide form
+JobsPreliminary <- pivot_wider(data = JobsPrelim,
+                                      names_from = NACE2007,
+                                      values_from = value,
+                                      names_prefix = "NA")
+
+# Write a csv
+write.csv(JobsPreliminary,
+          "~/JobsPreliminary.csv",
+          row.names = FALSE)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# DfToJdemetra
+# From Pandas DataFrame to Jdemetra+ with XML
+
+This repo documents an effort to make it easier to transform a Pandas DataFrame to an XML-file that can be used as input data for seasonal adjustment with [Jdemetra+](https://jdemetradocumentation.github.io/JDemetra-documentation/) and [JWSACruncher](https://github.com/jdemetra/jwsacruncher/wiki). The XML is then compliant with the specification laid out on [p.15 of this document](https://ec.europa.eu/eurostat/cros/system/files/jdemetra_user_guide.pdf). 
+
